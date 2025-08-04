510436ce2666fff2c95d61c2a82d98d96045a031
RuixiangWu227 <wrx2026715093@gmail.com>
2025-08-04 17:37:02 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,25 +1,56 @@
-//your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
+let paramSets = [
+  { size: 50, lineWidth: 2, spacing: 150, angle: 0,             colorMode: 0 },
+  { size: 50, lineWidth: 2, spacing: 150, angle: Math.PI/4,     colorMode: 1 },
+  { size: 80, lineWidth: 3, spacing: 180, angle: Math.PI/8,     colorMode: 0 },
+  { size: 60, lineWidth: 4, spacing: 200, angle: Math.PI/6,     colorMode: 1 },
+  { size:100, lineWidth: 5, spacing: 250, angle: Math.PI/2,     colorMode: 0 },
+  { size: 40, lineWidth: 1, spacing: 120, angle: Math.PI/3,     colorMode: 1 },
+  { size: 70, lineWidth: 3, spacing: 160, angle: Math.PI/5,     colorMode: 0 },
+  { size: 90, lineWidth: 4, spacing: 220, angle: Math.PI/7,     colorMode: 1 },
+  { size:120, lineWidth: 6, spacing: 300, angle: Math.PI * 0.75, colorMode: 0 }
+];
 
-
-function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
-  
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
-
-  //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+function setup() {
+  createCanvas(2000, 1000);
+  noLoop();
 }
 
-function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+function draw() {
+  for (let i = 0; i < paramSets.length; i++) {
+    let p = paramSets[i];
+    background(255);
+    drawMotifGrid(p);
+    saveCanvas(`Wallpaper${i+1}`, 'png');
+  }
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+function drawMotifGrid(p) {
+  let cols = floor(width / p.spacing);
+  let rows = floor(height / p.spacing);
+  for (let ix = 0; ix < cols; ix++) {
+    for (let iy = 0; iy < rows; iy++) {
+      let x = ix * p.spacing + p.spacing/2;
+      let y = iy * p.spacing + p.spacing/2;
+      drawMotif(x, y, p);
+    }
+  }
+}
+
+function drawMotif(x, y, p) {
+  push();
+    translate(x, y);
+    rotate(p.angle);
+    stroke(0);
+    strokeWeight(p.lineWidth);
+    if (p.colorMode === 0) {
+      fill(200, 50, 50, 150);
+    } else {
+      noFill();
+    }
+    ellipse(0, 0, p.size);
+    for (let k = 0; k < 4; k++) {
+      rotate(HALF_PI);
+      line(0, p.size/2, 0, p.size/2 + p.lineWidth*5);
+    }
+  pop();
 }
